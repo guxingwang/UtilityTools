a73f4d0cbf1bd8eda06d2e7727984ab66d8186a9
one <one@DESKTOP-FMT0PIL>
2017-08-22 11:25:46 +0800

    添加项目文件。

---
+++ AutoShutdown/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AutoShutdown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(90, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(161, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Location = new System.Drawing.Point(257, 10);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(115, 23);
+            this.btnShutdown.TabIndex = 1;
+            this.btnShutdown.Text = "定时休眠";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "休眠时间";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(474, 179);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "马上休眠";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 230);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnShutdown);
+            this.Controls.Add(this.dateTimePicker1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动休眠程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AutoShutdown/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoShutdown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 是否开始
+        /// </summary>
+        bool isStart = false;
+
+        Timer timer1 = new Timer();
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            this.dateTimePicker1.Value = DateTime.Today.AddDays(1).AddSeconds(-1);
+            this.timer1.Interval = 1 * 1 * 1000;//时间间隔为5分钟一次
+            this.timer1.Tick += Timer1_Tick;
+        }
+                
+
+        private void btnShutdown_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show($"确定要在{this.dateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss")}休眠么?","提示框", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.StartOrStop();
+            }            
+        }
+
+
+        /// <summary>
+        ///  启动或者停止服务
+        /// </summary>
+        private void StartOrStop()
+        {
+            this.isStart = !this.isStart;
+            if (isStart)
+            {
+                this.btnShutdown.Text = "停止休眠";
+                this.AddText("启动自动休眠模式");
+                this.dateTimePicker1.Enabled = false;
+                this.timer1.Enabled = true;
+                this.timer1.Start();
+            }
+            else
+            {
+                this.btnShutdown.Text = "开始休眠";
+                this.AddText("停止自动休眠模式");
+                this.dateTimePicker1.Enabled = true;
+                this.timer1.Enabled = false;
+                this.timer1.Stop();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (this.isStart && this.dateTimePicker1.Value <= DateTime.Now)
+            {                
+                this.StartOrStop();
+                this.Dormancy();
+                //int time = 10;//定义10秒中之后系统自动关机
+
+                //var startInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+                //startInfo.UseShellExecute = false;
+                //startInfo.RedirectStandardInput = true;
+                //startInfo.RedirectStandardOutput = true;
+                //startInfo.RedirectStandardError = true;
+                //startInfo.CreateNoWindow = true;
+                //var myProcess = new System.Diagnostics.Process();
+                //myProcess.StartInfo = startInfo;
+                //myProcess.Start();
+                //// myProcess.StandardInput.WriteLine("shutdown -s -t " + time);//自动关机
+                //myProcess.StandardInput.WriteLine("shutdown -h");
+            }
+        }
+
+        /// <summary>
+        /// 添加信息到文本框的最后方
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
+        private void AddText(string msg)
+        {
+            this.textBox1.AppendText($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}:{msg}{Environment.NewLine}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确定要在马上休眠么?", "提示框", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                this.Dormancy();
+            }
+                
+        }
+
+        /// <summary>
+        /// 休眠计算机
+        /// </summary>
+        private void Dormancy()
+        {
+            var startInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.CreateNoWindow = true;
+            var myProcess = new System.Diagnostics.Process();
+            myProcess.StartInfo = startInfo;
+            myProcess.Start();
+            // myProcess.StandardInput.WriteLine("shutdown -s -t " + time);//自动关机
+            myProcess.StandardInput.WriteLine("shutdown -h");
+        }
+    }
+}
